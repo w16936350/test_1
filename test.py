5417524bb4503ea9b01f5f0bdf6e8e805483ee70
w16936350 <84952422+w16936350@users.noreply.github.com>
2021-05-28 12:33:19 +0800

    add a test file

---
+++ test.py
@@ -0,0 +1,14 @@
+# # 1.7
+# from turtle import *
+# fillcolor("red")
+# begin_fill()
+# while True:
+#     forward(200)
+#     right(90)
+#     if abs(pos()) < 1:
+#         break
+# end_fill()
+# done()
+
+dic = {"zhong": "shang", "zhong": "xia"}
+print(dic['zhong'])
